0af786d956ef2fb76cda02ffcb1426ecf11c2a7b
isabella7565 <101139102+isabella7565@users.noreply.github.com>
2022-05-27 16:35:50 -0400

    Added buttons, instructions and lobby music

--- HomeWorld.java
+++ HomeWorld.java
@@ -17,5 +17,26 @@ public class HomeWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        prepare();
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        info info = new info();
+        addObject(info,91,299);
+        playbutton playbutton = new playbutton();
+        addObject(playbutton,282,72);
+        playbutton.setLocation(296,88);
+        playbutton.setLocation(319,81);
+        info.setLocation(105,305);
+        info.setLocation(84,323);
+        playbutton.setLocation(323,79);
+        removeObject(playbutton);
+        info.setLocation(320,64);
+        Greenfoot.playSound("a-jazz-piano-110481.mp3");
     }
 }
