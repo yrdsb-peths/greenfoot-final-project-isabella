8d0db1674ba2440c8a7257c6b26bf8bbbbd49f6d
isabella7565 <101139102+isabella7565@users.noreply.github.com>
2022-05-27 15:53:57 -0400

    Creation of worlds

---
+++ HomeWorld.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class HomeWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HomeWorld extends World
+{
+
+    /**
+     * Constructor for objects of class HomeWorld.
+     * 
+     */
+    public HomeWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
